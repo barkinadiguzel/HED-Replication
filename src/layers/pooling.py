3c5f260e6780aa519b287458992aff26ae92ba35
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-24 10:56:04 +0300

    Create pooling.py

---
+++ src/layers/pooling.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class MaxPool(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        return self.pool(x)
